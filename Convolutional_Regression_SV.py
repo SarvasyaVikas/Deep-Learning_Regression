6c3c95cdd6f77d479b276d2b7f7d0cf6cf0ceb3d
SarvasyaVikas <85037039+SarvasyaVikas@users.noreply.github.com>
2021-06-15 13:57:49 -0400

    Convolutional_Regression_SV- Description
    This program takes in a dataset of house images and applies regression with a convolutional neural network to predict the price of houses in the dataset.

---
+++ Convolutional_Regression_SV.py
@@ -0,0 +1,156 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import glob
+import cv2
+import numpy as np
+import argparse
+import locale
+import os
+
+def load_house_attributes(inputPath):
+	cols = ["bedrooms", "bathrooms", "area", "zipcode", "price"]
+	df = pd.read_csv(inputPath, sep=" ", header=None, names=cols)
+	zipcodes = df["zipcode"].value_counts().keys().tolist()
+	counts = df["zipcode"].value_counts().tolist()
+
+	for (zipcode, count) in zip(zipcodes, counts):
+		if count < 25:
+			idxs = df[df["zipcode"] == zipcode].index
+			df.drop(idxs, inplace=True)
+
+	return df
+
+def process_house_attributes(df, train, test):
+	continuous = ["bedrooms", "bathrooms", "area"]
+
+	cs = MinMaxScaler()
+	trainContinuous = cs.fit_transform(train[continuous])
+	testContinuous = cs.transform(test[continuous])
+
+	zipBinarizer = LabelBinarizer().fit(df["zipcode"])
+	trainCategorical = zipBinarizer.transform(train["zipcode"])
+	testCategorical = zipBinarizer.transform(test["zipcode"])
+
+	trainX = np.hstack([trainCategorical, trainContinuous])
+	testX = np.hstack([testCategorical, testContinuous])
+
+	return (trainX, testX)
+
+def load_house_images(df, inputPath):
+	images = []
+
+	for i in df.index.values:
+		basePath = os.path.sep.join([inputPath, "{}_*".format(i + 1)])
+		housePaths = sorted(list(glob.glob(basePath)))
+
+		inputImages = []
+		outputImage = np.zeros((64, 64, 3), dtype="uint8")
+
+		for housePath in housePaths:
+
+			image = cv2.imread(housePath)
+			image = cv2.resize(image, (32, 32))
+			inputImages.append(image)
+
+		outputImage[0:32, 0:32] = inputImages[0]
+		outputImage[0:32, 32:64] = inputImages[1]
+		outputImage[32:64, 32:64] = inputImages[2]
+		outputImage[32:64, 0:32] = inputImages[3]
+
+		images.append(outputImage)
+
+	return np.array(images)
+
+def create_mlp(dim, regress=False):
+	model = Sequential()
+	model.add(Dense(8, input_dim=dim, activation="relu"))
+	model.add(Dense(4, activation="relu"))
+
+	if regress:
+		model.add(Dense(1, activation="linear"))
+
+	return model
+
+def create_cnn(width, height, depth, filters=(16, 32, 64), regress=False):
+	inputShape = (height, width, depth)
+	chanDim = -1
+
+	inputs = Input(shape=inputShape)
+
+	for (i, f) in enumerate(filters):
+		if i == 0:
+			x = inputs
+
+		x = Conv2D(f, (3, 3), padding="same")(x)
+		x = Activation("relu")(x)
+		x = BatchNormalization(axis=chanDim)(x)
+		x = MaxPooling2D(pool_size=(2, 2))(x)
+
+	x = Flatten()(x)
+	x = Dense(16)(x)
+	x = Activation("relu")(x)
+	x = BatchNormalization(axis=chanDim)(x)
+	x = Dropout(0.5)(x)
+
+	x = Dense(4)(x)
+	x = Activation("relu")(x)
+
+	if regress:
+		x = Dense(1, activation="linear")(x)
+
+	model = Model(inputs, x)
+	
+	return model
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", type=str, required=True)
+args = vars(ap.parse_args())
+
+print("[INFO] loading house attributes...")
+inputPath = os.path.sep.join([args["dataset"], "HousesInfo.txt"])
+df = load_house_attributes(inputPath)
+
+print("[INFO] loading house images...")
+images = load_house_images(df, args["dataset"])
+images = images / 255.0
+
+split = train_test_split(df, images, test_size=0.25, random_state=42)
+(trainAttrX, testAttrX, trainImagesX, testImagesX) = split
+
+maxPrice = trainAttrX["price"].max()
+trainY = trainAttrX["price"] / maxPrice
+testY = testAttrX["price"] / maxPrice
+
+model = create_cnn(64, 64, 3, regress=True)
+opt = Adam(lr=1e-3, decay=1e-3 / 200)
+model.compile(loss="mean_absolute_percentage_error", optimizer=opt)
+
+print("[INFO] training model...")
+model.fit(x=trainImagesX, y=trainY, validation_data=(testImagesX, testY), epochs=200, batch_size=8)
+
+print("[INFO] predicting house prices...")
+preds = model.predict(testImagesX)
+
+diff = preds.flatten() - testY
+percentDiff = (diff / testY) * 100
+absPercentDiff = np.abs(percentDiff)
+
+mean = np.mean(absPercentDiff)
+std = np.std(absPercentDiff)
+
+locale.setlocale(locale.LC_ALL, "en_US.UTF-8")
+print("[INFO] avg. house price: {}, std house price: {}".format(locale.currency(df["price"].mean(), grouping=True), locale.currency(df["price"].std(), grouping=True)))
+print("[INFO] mean: {:.2f}%, std: {:.2f}%".format(mean, std))
